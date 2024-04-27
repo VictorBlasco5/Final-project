28c658256390a66305350bff4396d71a6fc663d2
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-27 09:38:37 +0200

    feat: userMatchSeeder created

---
+++ src/database/seeders/UserMatchSeeder.ts
@@ -0,0 +1,49 @@
+import { UserMatch } from "../../models/User_match";
+import { AppDataSource } from "../db";
+
+
+export const userMatchSeedDatabase = async () => {
+
+    try {
+        await AppDataSource.initialize();
+        
+        const court1 = UserMatch.create({
+            user: {id: 1},
+            match: {id: 4}
+        });
+        await court1.save();
+
+        const court2 = UserMatch.create({
+            user: {id: 5},
+            match: {id: 3}
+        });
+        await court2.save();
+
+        const court3 = UserMatch.create({
+            user: {id: 4},
+            match: {id: 1}
+        });
+        await court3.save();
+
+        const court4 = UserMatch.create({
+            user: {id: 3},
+            match: {id: 5}
+        });
+        await court4.save();
+
+        const court5 = UserMatch.create({
+            user: {id: 2},
+            match: {id: 2}
+        });
+        await court5.save();
+        
+  
+        console.log('---------------------------');
+        console.log('User_matches successfully saved');    
+        console.log('---------------------------');    
+      } catch (error) {
+        console.log(error);
+      } finally {
+        await AppDataSource.destroy()
+      }
+}
--- src/database/seeders/seeders.ts
+++ src/database/seeders/seeders.ts
@@ -2,12 +2,14 @@ import { roleSeedDatabase } from "./RoleSeeder";
 import { userSeedDatabase } from "./UserSeeder";
 import { courtSeedDatabase } from "./CourtSeeder";
 import { matchSeedDatabase } from "./MatchSeeder";
+import { userMatchSeedDatabase } from "./UserMatchSeeder";
 
 const launchSeeders = async () => {
     await roleSeedDatabase();
     await userSeedDatabase();
     await courtSeedDatabase();
     await matchSeedDatabase();
+    await userMatchSeedDatabase();
 }
 
 launchSeeders();
