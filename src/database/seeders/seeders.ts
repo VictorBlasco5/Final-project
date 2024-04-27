345220e4e38bfb98fb3d065fbd46b02729a183cc
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-27 09:49:40 +0200

    feat: favoriteCourtsSeeders created

--- src/database/seeders/seeders.ts
+++ src/database/seeders/seeders.ts
@@ -3,13 +3,16 @@ import { userSeedDatabase } from "./UserSeeder";
 import { courtSeedDatabase } from "./CourtSeeder";
 import { matchSeedDatabase } from "./MatchSeeder";
 import { userMatchSeedDatabase } from "./UserMatchSeeder";
+import { favoriteCourtSeedDatabase } from "./FavoriteCourtsSeeder";
+
 
 const launchSeeders = async () => {
-    await roleSeedDatabase();
-    await userSeedDatabase();
-    await courtSeedDatabase();
-    await matchSeedDatabase();
-    await userMatchSeedDatabase();
+    // await roleSeedDatabase();
+    // await userSeedDatabase();
+    // await courtSeedDatabase();
+    // await matchSeedDatabase();
+    // await userMatchSeedDatabase();
+    await favoriteCourtSeedDatabase();
 }
 
 launchSeeders();
