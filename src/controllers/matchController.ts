a38f20bb317a6f9e3f9174c7b527033a301cfaab
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-29 12:50:15 +0200

    feat: createMatch created in matchController

--- src/controllers/matchController.ts
+++ src/controllers/matchController.ts
@@ -6,14 +6,8 @@ export const getMatches = async (req: Request, res: Response) => {
     try {
 
         const matches = await Match.find({
-
-            select: {
-                id: true,
-                number_players: true,
-                information: true,
-                match_date: true,
-            }
-        })
+            relations: ["court"]
+        });
 
         res.status(200).json(
             {
@@ -27,6 +21,34 @@ export const getMatches = async (req: Request, res: Response) => {
         res.status(500).json({
             success: false,
             message: "Matches cant be retrieved",
+            error: error
+        })
+    }
+}
+
+export const createMatch = async (req: Request, res: Response) => {
+    try {
+        const { number_players, information, match_date, court_id } = req.body;
+
+        const newMatch = new Match();
+        newMatch.number_players = number_players;
+        newMatch.information = information;
+        newMatch.match_date = match_date;
+        newMatch.court = court_id;
+        await newMatch.save();
+        
+        res.status(200).json(
+            {
+                succes: true,
+                message: "Match created",
+                data: newMatch
+            }
+        )
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Cant create match",
             error: error
         })
     }
