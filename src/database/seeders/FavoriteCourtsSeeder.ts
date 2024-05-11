d808b8066f5e1e89ff7609e81943e20038aad1e9
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-05-11 16:24:13 +0200

    update: increase seeders

--- src/database/seeders/FavoriteCourtsSeeder.ts
+++ src/database/seeders/FavoriteCourtsSeeder.ts
@@ -8,28 +8,28 @@ export const favoriteCourtSeedDatabase = async () => {
         await AppDataSource.initialize();
         
         const favorite1 = FavoriteCourt.create({
-            name: "Pistes de bàsquet CAVALIERS",
+            name: "Cavaliers",
             user: {id: 1},
             court: {id: 4}
         });
         await favorite1.save();
         
         const favorite2 = FavoriteCourt.create({
-            name: "Pabellón Fuente de San Luis",
+            name: "Tarongers",
             user: {id: 4},
             court: {id: 1}
         });
         await favorite2.save();
         
         const favorite3 = FavoriteCourt.create({
-            name: "Pista de basket Villar Palasí",
+            name: "Villar Palasí",
             user: {id: 2},
             court: {id: 5}
         });
         await favorite3.save();
         
         const favorite4 = FavoriteCourt.create({
-            name: "Pistes de bàsquet CAVALIERS",
+            name: "Malvarrosa",
             user: {id: 2},
             court: {id: 4}
         });
@@ -41,8 +41,77 @@ export const favoriteCourtSeedDatabase = async () => {
             court: {id: 2}
         });
         await favorite5.save();
-        
-  
+
+        const favorite6 = FavoriteCourt.create({
+            name: "Campanar",
+            user: {id: 3},
+            court: {id: 1}
+        });
+        await favorite6.save();
+
+        const favorite7 = FavoriteCourt.create({
+            name: "Jardín del Turia",
+            user: {id: 1},
+            court: {id: 5}
+        });
+        await favorite7.save();
+
+        const favorite8 = FavoriteCourt.create({
+            name: "Polideportivo Mislata",
+            user: {id: 3},
+            court: {id: 2}
+        });
+        await favorite8.save();
+
+        const favorite9 = FavoriteCourt.create({
+            name: "Pistons",
+            user: {id: 5},
+            court: {id: 1}
+        });
+        await favorite9.save();
+
+        const favorite10 = FavoriteCourt.create({
+            name: "La Saïdia",
+            user: {id: 4},
+            court: {id: 5}
+        });
+        await favorite10.save();
+
+        const favorite11 = FavoriteCourt.create({ 
+            name: "Villar Palasí",
+            user: {id: 1},
+            court: {id: 5}
+        });
+        await favorite11.save();
+
+        const favorite12 = FavoriteCourt.create({
+            name: "Polideportivo Mislata",
+            user: {id: 2},
+            court: {id: 2}
+        });
+        await favorite12.save();
+
+        const favorite13 = FavoriteCourt.create({
+            name: "Polideportivo Municipal de Picanya",
+            user: {id: 3},
+            court: {id: 1}
+        });
+        await favorite13.save();
+
+        const favorite14 = FavoriteCourt.create({
+            name: "Jardín del Turia",
+            user: {id: 4},
+            court: {id: 5}
+        });
+        await favorite14.save();
+
+        const favorite15 = FavoriteCourt.create({
+            name: "Malvarrosa",
+            user: {id: 5},
+            court: {id: 2}
+        });
+        await favorite15.save();
+
         console.log('---------------------------');
         console.log('Favorite_courts successfully saved');    
         console.log('---------------------------');    
--- src/database/seeders/UserSeeder.ts
+++ src/database/seeders/UserSeeder.ts
@@ -65,7 +65,125 @@ export const userSeedDatabase = async () => {
       });
       await user5.save();
 
-      
+      const user6 = User.create({
+        name: "Carlos",
+        nickname: "Carlos",
+        email: "carlos@carlos.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Base",
+        presentation: "Hola me llamo Carlos y me gusta jugar a baloncesto 6",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user6.save();
+
+      const user7 = User.create({
+        name: "Javier",
+        nickname: "Javier",
+        email: "javier@javier.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Base",
+        presentation: "Hola me llamo Javier y me gusta jugar a baloncesto 7",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user7.save();
+
+      const user8 = User.create({
+        name: "Marta",
+        nickname: "Marta",
+        email: "marta@marta.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Alero",
+        presentation: "Hola me llamo Marta y me gusta jugar a baloncesto 8",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user8.save();
+
+      const user9 = User.create({
+        name: "Sara",
+        nickname: "Sara",
+        email: "sara@sara.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Base",
+        presentation: "Hola me llamo Sara y me gusta jugar a baloncesto 9",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user9.save();
+
+      const user10 = User.create({
+        name: "Pablo",
+        nickname: "Pablo",
+        email: "pablo@pablo.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Pívot",
+        presentation: "Hola me llamo Pablo y me gusta jugar a baloncesto 10",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user10.save();
+
+      const user11 = User.create({
+        name: "María",
+        nickname: "María",
+        email: "maria@maria.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Base",
+        presentation: "Hola me llamo María y me gusta jugar a baloncesto 11",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user11.save();
+
+      const user12 = User.create({
+        name: "Ana",
+        nickname: "Ana",
+        email: "ana@ana.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Alero",
+        presentation: "Hola me llamo Ana y me gusta jugar a baloncesto 12",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user12.save();
+
+      const user13 = User.create({
+        name: "Lucía",
+        nickname: "Lucía",
+        email: "lucia@lucia.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Pivot",
+        presentation: "Hola me llamo Lucía y me gusta jugar a baloncesto 13",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user13.save();
+
+      const user14 = User.create({
+        name: "Miguel",
+        nickname: "Miguel",
+        email: "miguel@miguel.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Base",
+        presentation: "Hola me llamo Miguel y me gusta jugar a baloncesto 14",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user14.save();
+
+      const user15 = User.create({
+        name: "Jorge",
+        nickname: "Jorge",
+        email: "jorge@jorge.com",
+        password: "$2b$08$ZEsDerEChlJlGCodM7tIYO1Jtp6uXfajfZNByeNQ1rUBnlI3ovIMm",
+        favorite_position: "Pivot",
+        presentation: "Hola me llamo Jorge y me gusta jugar a baloncesto 15",
+        image: "https://www.dzoom.org.es/wp-content/uploads/2020/02/portada-foto-perfil-redes-sociales-consejos-810x540.jpg",
+        role: {id: 1}
+      });
+      await user15.save(); 
       
       console.log('---------------------------');
       console.log('Users successfully saved');    
